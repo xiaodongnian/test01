35669021c5696ac65f26afd2b0c172d3e6d1d87d
xiaodongnian <2134048835@qq.com>
2023-05-05 18:01:41 +0800

    力扣第一题两数之和

---
+++ src/zzz.java
@@ -0,0 +1,39 @@
+import java.lang.reflect.Array;
+import java.sql.SQLOutput;
+import java.util.Arrays;
+
+public class zzz {
+    public static void main(String[] args) {
+        int[] nums = new int[]{2, 3, 7, 9};
+        int target = 9;
+        System.out.println(Arrays.toString(xiangjia(nums, target)));
+
+    }
+
+    public static int[] xiangjia(int[] nums, int target) {
+
+
+        int n = nums.length;
+        int[] k = new int[2];
+
+        for (int i = 0; i < n; i++) {
+            for (int j = i + 1; j < n; j++) {
+                if (nums[i] + nums[j] == target) {
+
+                    k[0] = i;
+                    k[1] = j;
+                    return k;
+                }
+
+
+            }
+
+
+        }
+        return null;
+
+    }
+
+}
+
+
