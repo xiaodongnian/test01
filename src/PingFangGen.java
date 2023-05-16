291db246f70631037ca2705b8c3a5964a8b2d89e
xiaodongnian <2134048835@qq.com>
2023-05-16 13:59:40 +0800

    求平方根

--- src/PingFangGen.java
+++ src/PingFangGen.java
@@ -9,7 +9,7 @@ public class PingFangGen {
         int l=0,r=x,ans=0;
         while(l<=r){
             int mid=l+(r-l)/2;
-            if(mid*mid<=x){
+            if((long)mid*mid<=x){
                 ans=mid;
                 l=mid+1;
             }else{
