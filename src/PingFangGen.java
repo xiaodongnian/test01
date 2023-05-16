98e6dc65a4f629279720f63487e379d7f33d4459
xiaodongnian <2134048835@qq.com>
2023-05-16 13:56:07 +0800

    求平方根

---
+++ src/PingFangGen.java
@@ -0,0 +1,20 @@
+public class PingFangGen {
+    public static void main(String[] args) {
+        PingFangGen pingFangGen=new PingFangGen();
+
+        int x=9;
+        System.out.println(pingFangGen.uu(x));
+
+    }public int uu(int x){
+        int l=0,r=x,ans=0;
+        while(l<=r){
+            int mid=l+(r-l)/2;
+            if(mid*mid<=x){
+                ans=mid;
+                l=mid+1;
+            }else{
+                r=mid-1;
+            }
+        }return ans;
+    }
+}
