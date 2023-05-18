4290622f91b5966d4fc47fdfda5a109f38594979
xiaodongnian <2134048835@qq.com>
2023-05-18 12:47:44 +0800

    删除链表

---
+++ src/Shanchu.java
@@ -0,0 +1,44 @@
+public class Shanchu {
+    public static void main(String[] args) {
+        ListNode head=new ListNode(3,new ListNode(3,new ListNode(4,new ListNode(5,new ListNode(5)))));
+
+
+
+        ListNode g= deleteDuplicates(head);
+        while(g!=null){
+            System.out.print(g.val+" ");
+            g=g.next;
+        }
+
+
+
+}public  static ListNode deleteDuplicates(ListNode head) {
+        ListNode o=head;
+        while(o.next!=null){
+            if(o.next.val==o.val){
+                o.next=o.next.next;
+            }else {
+                o=o.next;
+
+            }
+
+        }return head;
+
+
+    }
+public static  class ListNode {
+    int val;
+    ListNode next;
+
+    ListNode() {
+    }
+
+    ListNode(int val) {
+        this.val = val;
+    }
+
+    ListNode(int val, ListNode next) {
+        this.val = val;
+        this.next = next;
+    }
+}}
