efbacdef521934958a18162f2b8704e0d61ea352
xiaodongnian <2134048835@qq.com>
2023-05-21 10:45:56 +0800

    杨辉三角1

---
+++ src/YanghuiSanjiao.java
@@ -0,0 +1,71 @@
+import java.awt.*;
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.lang.reflect.Array;
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+
+public class YanghuiSanjiao {
+    public static String integerArrayListToString(List<Integer> nums, int length) {
+        if (length == 0) {
+            return "[]";
+        }
+
+        String result = "";
+        for(int index = 0; index < length; index++) {
+            Integer number = nums.get(index);
+            result += Integer.toString(number) + ", ";
+        }
+        return "[" + result.substring(0, result.length() - 2) + "]";
+    }
+
+    public static String integerArrayListToString(List<Integer> nums) {
+        return integerArrayListToString(nums, nums.size());
+    }
+
+    public static String int2dListToString(List<List<Integer>> nums) {
+        StringBuilder sb = new StringBuilder("[");
+        for (List<Integer> list: nums) {
+            sb.append(integerArrayListToString(list));
+            sb.append(",");
+        }
+
+        sb.setCharAt(sb.length() - 1, ']');
+        return sb.toString();
+    }
+
+    public static void main(String[] args) throws IOException {
+        BufferedReader in = new BufferedReader(new InputStreamReader(System.in));
+        String line;
+        while ((line = in.readLine()) != null) {
+            int numRows = Integer.parseInt(line);
+
+            List<List<Integer>> ret = new Solution().generate(numRows);
+
+            String out = int2dListToString(ret);
+
+            System.out.print(out);
+        }
+
+    }
+    private static class Solution {
+        public List<List<Integer>> generate(int numRows) {
+            List<List<Integer>> ret = new ArrayList<List<Integer>>();
+            for (int i = 0; i < numRows; ++i) {
+                List<Integer> row = new ArrayList<Integer>();
+                for (int j = 0; j <= i; ++j) {
+                    if (j == 0 || j == i) {
+                        row.add(1);
+                    } else {
+                        row.add(ret.get(i - 1).get(j - 1) + ret.get(i - 1).get(j));
+                    }
+                }
+                ret.add(row);
+            }
+            return ret;
+        }
+    }
+}
+
