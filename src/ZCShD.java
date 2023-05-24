2a7331f365a73948124a2e8ac7057e2169b259f1
xiaodongnian <2134048835@qq.com>
2023-05-24 17:47:30 +0800

    二叉树系列

---
+++ src/ZCShD.java
@@ -0,0 +1,26 @@
+public class ZCShD {
+    public static void main(String[] args) {
+
+
+    }public int maxDepth(TreeNode root) {
+        if(root==null){
+            return 0;
+        }int leftheight=maxDepth(root.left);
+        int rightheight=maxDepth(root.right);
+        return Math.max(leftheight,rightheight)+1;
+
+
+    }
+
+public class TreeNode {
+     int val;
+     TreeNode left;
+     TreeNode right;
+     TreeNode() {}
+    TreeNode(int val) { this.val = val; }
+      TreeNode(int val, TreeNode left, TreeNode right) {
+         this.val = val;
+         this.left = left;
+         this.right = right;
+     }
+ }}
